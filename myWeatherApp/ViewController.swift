86cde6cb876f7285a1a8dfb9eca5d83743a8d793
Nikita <invisible24rus@gmail.com>
2022-01-30 20:35:24 +0300

    delete tableView

--- myWeatherApp/ViewController.swift
+++ myWeatherApp/ViewController.swift
@@ -9,14 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let weatherSearchController = UISearchController(searchResultsController: nil)
-    private let tableView = UITableView()
-    
+//    private let weatherSearchController = UISearchController(searchResultsController: nil)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tableView.dataSource = self
-//        tableView.delegate = self
         setupViews()
     }
 
@@ -29,26 +26,36 @@ private extension ViewController {
     
     func setupViews() {
         
-        view.addSubview(tableView)
-        tableView.backgroundColor = .white
-        tableView.separatorStyle = .singleLine
         
         
-        title = "Список городов"
-        navigationItem.searchController = weatherSearchController
-        weatherSearchController.searchResultsUpdater = self
-        weatherSearchController.searchBar.placeholder = "Поиск города"
+        view.addSubviewsForAutoLayout([])
+        
+//        title = "Список городов"
+//        navigationItem.searchController = weatherSearchController
+//        weatherSearchController.searchResultsUpdater = self
+//        weatherSearchController.searchBar.placeholder = "Поиск города"
+        
+
+        
     }
 }
 
 //MARK: - UISearchResultsUpdating
 
-extension ViewController: UISearchResultsUpdating {
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else { return }
-        
-        print(text)
-    }
-    
-}
+//extension ViewController: UISearchResultsUpdating {
+//
+//    func updateSearchResults(for searchController: UISearchController) {
+//        guard let text = searchController.searchBar.text else { return }
+//
+//        print(text)
+//    }
+//
+//}
+
+//MARK: - UITableViewDataSource
+
+
+
+//MARK: - UITableViewDelegate
+
+
